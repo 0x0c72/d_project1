bec0d2840f08db4e601687e11db40f7490da7682
Chris Shenkan <Chris Shenkan>
2013-12-02 16:02:51 -0500

    into part2 of tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from polls.models import Choice, Poll
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):	
 	model = Choice
 	extra = 3
 
